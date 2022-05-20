4450570c9bbcbf59db046bb62c76c6201724ba3d
Lazar-J <103836075+Lazar-J@users.noreply.github.com>
2022-05-20 12:11:53 +0200

    Make function computerPlay
    Its used to return random computer choice(rock, paper, scissors)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 window.onload = function() {
+
     console.log("connected");
+
+    computerPlay();
+}
+
+function computerPlay() {
+
+    let number = Math.random();
+    // console.log(number);
+    let roundedNumber = Math.floor(number * 3) + 1;
+    // console.log(roundedNumber);
+
+    let computerChoice;
+
+    switch(roundedNumber) {
+        case 1: computerChoice = "Rock";
+        break;
+        case 2: computerChoice ="Paper";
+        break;
+        case 3: computerChoice = "Scissors";
+        break;
+        default: computerChoice = "Impossible!";
+    }
+
+    console.log(computerChoice);
 }
