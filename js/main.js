4b0d94e67b9ff0d687dad6a5309c8d2d4274bce7
Lazar-J <103836075+Lazar-J@users.noreply.github.com>
2022-05-20 13:48:00 +0200

    Update function game and function playRound
    I added score to function game, so now we can display score, and we can show winner on the end.
    For winner of current round, I used return value of parameter winner in playRound function.

--- js/main.js
+++ js/main.js
@@ -51,42 +51,61 @@ function playerPlay() {
 
 function playRound(playerSelection, computerSelection) {
 
+    let winner = "";
+
     console.log(`You choosed ${playerSelection}. Computer choosed ${computerSelection}.`);
 
     if(playerSelection == computerSelection) {
         console.log(`Draw! ${playerSelection} cant beat ${computerSelection}.`);
-        
     }
     if(playerSelection == "Rock" && computerSelection == "Paper") {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        winner = "computer";
     }
     if(playerSelection == "Rock" && computerSelection == "Scissors") {
         console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        winner = "player";
     }
     if(playerSelection == "Paper" && computerSelection == "Rock") {
         console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        winner = "player";
     }
     if(playerSelection == "Paper" && computerSelection == "Scissors") {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        winner = "computer";
     }
     if(playerSelection == "Scissors" && computerSelection == "Rock") {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        winner = "computer";
     }
     if(playerSelection == "Scsissors" && computerSelection == "Paper") {
         console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        winner = "player";
     }
+
+    return winner;
 
 }
 
 function game() {
 
-    let playerScore;
-    let computerScore;
+    let playerScore = 0;
+    let computerScore = 0;
 
     for(let i = 0; i < 5; i++) {
         let playerSelection = playerPlay();
         let computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
+
+        let winner = playRound(playerSelection, computerSelection);
+
+        if(winner == "player") playerScore++;
+        if(winner == "computer") computerScore++;
+
+        console.log(`Player ${playerScore} - ${computerScore} Computer`);
     }
+
+    if(playerScore == computerScore) console.log("Draw. Maybe you should try again.")
+    if(playerScore > computerScore) console.log("You are the winner! Congratulations.");
+    if(computerScore > playerScore) console.log("You lost. Better luck next time.")
 
 }
